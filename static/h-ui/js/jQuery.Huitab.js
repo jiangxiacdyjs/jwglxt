16ae46040603bfb067f475ba06c8387c9dd9e3d2
freshzxf <370234987@qq.com>
2017-07-25 18:25:01 +0800

    食材编辑上报页面部分改动，未完善....

--- static/h-ui/js/jQuery.Huitab.js
+++ static/h-ui/js/jQuery.Huitab.js
@@ -1,13 +1,3 @@
-/* =======================================================================
- * jQuery.Huitab.js v2.0 选项卡
- * http://www.h-ui.net/
- * Created & Modified by guojunhui
- * Date modified 2017.05.05
- *
- * Copyright 2017 北京颖杰联创科技有限公司 All rights reserved.
- * Licensed under MIT license.
- * http://opensource.org/licenses/MIT
- * ========================================================================*/
 !function($) {
 	$.fn.Huitab = function(options){
 		var defaults = {
@@ -20,10 +10,10 @@
 		var options = $.extend(defaults, options);
 		this.each(function(){
 			var that = $(this);
-			that.find(options.tabBar).removeClass(options.className);
+			that.find(options.tabBar).removeClass(options.className);  //h4
 			that.find(options.tabBar).eq(options.index).addClass(options.className);
-			that.find(options.tabCon).hide();
-			that.find(options.tabCon).eq(options.index).show();
+			that.find(options.tabCon).hide();  //info
+			that.find(options.tabCon).eq(options.index).show();   //info
 			
 			that.find(options.tabBar).on(options.tabEvent,function(){
 				that.find(options.tabBar).removeClass(options.className);
